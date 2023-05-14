984eb9b2ca937c38d4d747fb4f97d18bfcb748ae
RoKerjea <rokerjea@student.42.fr>
2023-05-14 21:08:54 +0200

    Sub, Mul, Div for scalar added

--- src/main.rs
+++ src/main.rs
@@ -46,6 +46,7 @@ impl PartialEq<Tuple> for Tuple {
 
 impl ops::Add<Self> for Tuple {
 	type Output = Self;
+
 	fn add(self, rhs: Self) -> Self::Output {
 		Tuple::new(
             self.x + rhs.x,
@@ -53,6 +54,47 @@ impl ops::Add<Self> for Tuple {
             self.z + rhs.z,
             self.w + rhs.w,
         )
+	}
+}
+
+impl ops::Sub<Self> for Tuple {
+	type Output = Self;
+	fn sub(self, rhs: Self) -> Self{
+		Tuple::new(
+            self.x - rhs.x,
+            self.y - rhs.y,
+            self.z - rhs.z,
+            self.w - rhs.w,
+		)
+	}
+}
+impl ops::Mul<f64> for Tuple {
+	type Output = Self;
+	fn mul(self, rhs: f64) -> Self{
+		Tuple::new(
+            self.x * rhs,
+            self.y * rhs,
+            self.z * rhs,
+            self.w * rhs,
+		)
+	}
+}
+impl ops::Div<f64> for Tuple {
+	type Output = Self;
+	fn div(self, rhs: f64) -> Self{
+		self * (1.0 / rhs)
+	}
+}
+
+impl ops::Neg for Tuple {
+	type Output = Self;
+	fn neg(self) -> Self{
+		Tuple::new(
+            -self.x,
+            -self.y,
+            -self.z,
+           - self.w,
+		)
 	}
 }
 
@@ -110,5 +152,75 @@ mod	tests {
 		let tupleexpected = Tuple::new(1.0, 1.0, 6.0, 1.0);
 
 		assert_eq!(tuple1 + tuple2, tupleexpected);
+	}
+	#[test]
+	fn	points_substraction()
+	{
+		let tuple1 = Tuple::point(3.0, 2.0, 1.0);
+		let	tuple2 = Tuple::point(5.0, 6.0, 7.0);
+		let expected_tuple = Tuple::vector(-2.0, -4.0, -6.0);
+
+		assert_eq!(tuple1 - tuple2, expected_tuple);
+	}
+	#[test]
+	fn	vector_substraction_from_point()
+	{
+		let tuple1 = Tuple::point(3.0, 2.0, 1.0);
+		let	tuple2 = Tuple::vector(5.0, 6.0, 7.0);
+		let expected_tuple = Tuple::point(-2.0, -4.0, -6.0);
+
+		assert_eq!(tuple1 - tuple2, expected_tuple);
+	}
+	#[test]
+	fn	vectors_substraction()
+	{
+		let tuple1 = Tuple::vector(3.0, 2.0, 1.0);
+		let	tuple2 = Tuple::vector(5.0, 6.0, 7.0);
+		let expected_tuple = Tuple::vector(-2.0, -4.0, -6.0);
+
+		assert_eq!(tuple1 - tuple2, expected_tuple);
+	}
+	#[test]
+	fn	vector_substraction_from_zero_vector()
+	{
+		let tuple1 = Tuple::vector(0.0, 0.0, 0.0);
+		let	tuple2 = Tuple::vector(1.0, -2.0, 3.0);
+		let expected_tuple = Tuple::vector(-1.0, 2.0, -3.0);
+
+		assert_eq!(tuple1 - tuple2, expected_tuple);
+	}
+	#[test]
+	fn	negating_tuple()
+	{
+		let tuple1 = Tuple::new(1.0, -2.0, 3.0, -4.0);
+		let expected_tuple = Tuple::new(-1.0, 2.0, -3.0, 4.0);
+
+		assert_eq!(-tuple1, expected_tuple);
+	}
+	#[test]
+	fn	scalar_tuple_multiplication()
+	{
+		let tuple1 = Tuple::new(1.0, -2.0, 3.0, -4.0);
+		let expected_tuple = Tuple::new(3.5, -7.0, 10.5, -14.0);
+
+		assert_eq!(tuple1 * 3.5, expected_tuple);
+		// assert_eq!(3.5 * tuple1, expected_tuple);
+		//doesn't work because of order!
+	}
+	#[test]
+	fn	fraction_scalar_tuple_multiplication()
+	{
+		let tuple1 = Tuple::new(1.0, -2.0, 3.0, -4.0);
+		let expected_tuple = Tuple::new(0.5, -1.0, 1.5, -2.0);
+
+		assert_eq!(tuple1 * 0.5, expected_tuple);
+	}
+	#[test]
+	fn	scalar_tuple_division()
+	{
+		let tuple1 = Tuple::new(1.0, -2.0, 3.0, -4.0);
+		let expected_tuple = Tuple::new(0.5, -1.0, 1.5, -2.0);
+
+		assert_eq!(tuple1 / 2.0, expected_tuple);
 	}
 }
